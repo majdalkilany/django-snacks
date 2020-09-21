5fdce46cc403782abbfcf07dfa9350d27701fe1d
majdalkilany <majdkilany92@gmail.com>
2020-09-21 13:35:05 +0300

    test about page

--- snacks/tests.py
+++ snacks/tests.py
@@ -17,3 +17,16 @@ class SnacksTest(SimpleTestCase):
         response = self.client.get(url)
         actual = 'home.html'
         self.assertTemplateUsed(response, actual)
+
+    def test_about_page_status_code(self):
+        expected = 200
+        url = reverse('about')
+        response = self.client.get(url)
+        actual = response.status_code 
+        self.assertEquals(expected, actual)
+
+    def test_about_page_template(self):
+        url = reverse('about')
+        response = self.client.get(url)
+        actual = 'about.html'
+        self.assertTemplateUsed(response, actual)
